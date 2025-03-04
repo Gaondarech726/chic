1f0008ef105816b3bbdb6795eea044aa827148ab
Gaondarech726 <123805187+Gaondarech726@users.noreply.github.com>
2025-03-04 16:27:22 +0000

    Deploying to gh-pages from @ Gaondarech726/chic@29c77000680b7a892a11d20a9f9ed78721c1a330 🚀

---
+++ static/js/736.e7f56b85.chunk.js
@@ -0,0 +1,41 @@
+"use strict";(self.webpackChunkchic=self.webpackChunkchic||[]).push([[736],{231:(o,n,t)=>{t.r(n),t.d(n,{H1:()=>f,H2:()=>w,Li:()=>z,Main:()=>j,P:()=>u,Ul:()=>P,default:()=>Z});var r=t(867),i=(t(666),t(961)),e=t(626),s=t(42),p=t(500),c=t(559),a=t.n(c),l=t(93),d=t(858),x=t(184);const h=new(a().Icon)({iconUrl:l,shadowUrl:d,iconSize:[25,41],iconAnchor:[12,41],popupAnchor:[1,-34]}),g=r.ZP.h1`
+  color: rgb(124, 16, 119);
+  padding-bottom: 50px;
+  padding-top: 50px;
+`,b=[{name:"CHIC",description:"\u0427\u0435\u043a\u0430\u0454\u043c\u043e \u043d\u0430 \u0412\u0430\u0441 \u0442\u0443\u0442",lat:50.4443419297771,lon:30.415234477297854}];function m(){return(0,x.jsxs)(x.Fragment,{children:[(0,x.jsx)(g,{children:"\u041d\u0430\u0448\u0435 \u043c\u0456\u0441\u0446\u0435 \u0440\u043e\u0431\u043e\u0442\u0438"}),(0,x.jsxs)(i.h,{center:[50.4443419297771,30.415234477297854],zoom:13,className:"map-container",children:[(0,x.jsx)(e.I,{attribution:'\xa9 <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',url:"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}),b.map(((o,n)=>(0,x.jsx)(s.J,{position:[o.lat,o.lon],icon:h,children:(0,x.jsxs)(p.G,{children:[(0,x.jsx)("strong",{children:o.name})," ",(0,x.jsx)("br",{})," ",(0,x.jsxs)("b",{children:[" ",o.description]})]})},n)))]})]})}const j=r.ZP.main`
+  margin-top: 50px;
+`,u=r.ZP.p`
+  color: rgb(124, 16, 119);
+  padding-bottom: 50px;
+  font-size: 20px;
+  @media (max-width: 425px) {
+    font-size: 16px;
+  }
+`,f=r.ZP.h1`
+  color: rgb(124, 16, 119);
+  padding-bottom: 50px;
+`,w=r.ZP.h2`
+  padding-bottom: 20px;
+  color: rgb(124, 16, 119);
+`,P=r.ZP.ul`
+  color: rgb(124, 16, 119);
+  padding-bottom: 50px;
+`,z=r.ZP.li`
+  font-size: 20px;
+  @media (max-width: 425px) {
+    font-size: 16px;
+  }
+  padding-bottom: 25px;
+  color: rgb(124, 16, 119);
+
+  list-style: none;
+  > a {
+    text-decoration: none;
+    transition: color 0.5s ease;
+    color: rgb(255, 0, 0);
+    &:hover {
+      color: rgb(255, 103, 103);
+    }
+  }
+`,Z=()=>(0,x.jsxs)(j,{children:[(0,x.jsx)(f,{}),(0,x.jsx)(u,{children:(0,x.jsx)("br",{})}),(0,x.jsx)(m,{})]})}}]);
+//# sourceMappingURL=736.e7f56b85.chunk.js.map
